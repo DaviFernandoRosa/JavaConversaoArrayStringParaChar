ce612553beedee5b1ec5afa6e69736815c6b8366
Davi Fernando Rosa <davir0514@gmail.com>
2021-06-25 22:37:45 -0300

    Arquivo

---
+++ App.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class App {
+
+    public static void main (String [] args){
+     
+
+        String nome= "Bruno";
+        for(char c:nome.toCharArray()){
+            System.out.printf("%c" , c);
+        }
+     
+        }
+
+       
+    }
